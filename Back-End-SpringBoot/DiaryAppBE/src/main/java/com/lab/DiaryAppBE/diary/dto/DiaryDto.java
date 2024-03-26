3134fca532e3d843b86159e33554a50f9907069f
Sooyeon Noh <tndus1739@gmail.com>
2024-03-26 18:24:19 +0900

    com

---
+++ Back-End-SpringBoot/DiaryAppBE/src/main/java/com/lab/DiaryAppBE/diary/dto/DiaryDto.java
@@ -0,0 +1,30 @@
+package com.lab.DiaryAppBE.diary.dto;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryDto {
+
+    private Long id;
+	
+	private String content;
+	
+	private int emotionId;
+	
+	private String Date;
+	
+	
+	
+	
+}
